ad6ed494aae29d60940f9309a3e43b151c1a809c
mrbluek79 <84831138+mrbluek79@users.noreply.github.com>
2022-09-04 16:02:48 +0900

    Initial commit

---
+++ src/main/java/com/musinsa/assign/MusinsaAssignApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.assign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaAssignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaAssignApplication.class, args);
+    }
+
+}
